025e7805c2871d8cdb71937a7f9171a5f28df9b7
Sherwin Saga <sherwinnikolaisaga@gmail.com>
2023-05-16 19:09:55 +0800

    milestone
    ->more settings ui (fragment_more & fragment_more.xml)
    ->working dark mode switch

--- app/src/main/java/com/example/novelfolio/Fragment/FragmentMore.java
+++ app/src/main/java/com/example/novelfolio/Fragment/FragmentMore.java
@@ -1,22 +1,53 @@
 package com.example.novelfolio.Fragment;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 
 import com.example.novelfolio.R;
 
 public class FragmentMore extends Fragment {
 
+    private Switch switcher;
+    private boolean nightMode;
+    private SharedPreferences sharedPreferences;
+    private SharedPreferences.Editor editor;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_more, container, false);
+        View view = inflater.inflate(R.layout.fragment_more, container, false);
+
+        switcher = view.findViewById(R.id.darkModeSwitch);
+        nightMode = AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES;
+
+        switcher.setChecked(nightMode);
+
+        switcher.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                nightMode = !nightMode;
+                if (nightMode) {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                } else {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                }
+
+            }
+        });
+
+        return view;
     }
 }
