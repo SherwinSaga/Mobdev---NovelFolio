4ae018fb1099f25ab115ff5deb7df0b12f95c5ba
Jhudiel Muñoz <67956800+jhudiel0101@users.noreply.github.com>
2023-05-16 18:18:22 +0800

    Add login logic

--- app/src/main/java/com/example/novelfolio/MainActivity.java
+++ app/src/main/java/com/example/novelfolio/MainActivity.java
@@ -1,31 +1,59 @@
 package com.example.novelfolio;
 
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 
 
 public class MainActivity extends AppCompatActivity {
-
+    private FirebaseAuth mAuth;
     Button buttonLOGIN;
+    private EditText email;
+    private EditText password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mAuth = FirebaseAuth.getInstance();
+
         buttonLOGIN = findViewById(R.id.btnLogin);
+        email = findViewById(R.id.editTextEmail);
+        password = findViewById(R.id.editTextPassword);
 
         buttonLOGIN.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this, MainScreen.class);
                 startActivity(intent);
+                mAuth.signInWithEmailAndPassword(email.getText().toString(), password.getText().toString())
+                        .addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    // Sign-in successful
+                                    Toast.makeText(MainActivity.this, "Sign-in successful", Toast.LENGTH_SHORT).show();
+                                    // Proceed with your desired logic
+                                } else {
+                                    // Sign-in failed
+                                    Toast.makeText(MainActivity.this, "Sign-in failed", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
             }
         });
     }
