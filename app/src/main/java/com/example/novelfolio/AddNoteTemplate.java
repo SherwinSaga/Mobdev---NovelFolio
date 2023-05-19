63da7ddaad24d427303c79fceca88a012453e29d
Sherwin Saga <sherwinnikolaisaga@gmail.com>
2023-05-19 11:52:26 +0800

    milestone ->add note template ->view ntoe template

---
+++ app/src/main/java/com/example/novelfolio/AddNoteTemplate.java
@@ -0,0 +1,14 @@
+package com.example.novelfolio;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddNoteTemplate extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_note_template);
+    }
+}
